b88f1ea0fdc641c5dea07987545a9165d26455f0
JavierQuinan <javierquiandra@gmail.com>
2025-01-06 19:34:43 -0500

    Añadido soporte para versión automática de React en ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,26 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+            globals: globals.browser,
+        },
+        plugins: {
+            react: pluginReact,
+            js: pluginJs,
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            ...pluginReact.configs.flat.recommended.rules
+        }
+    }
 ];

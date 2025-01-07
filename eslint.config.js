d0d4822860c5819fa27214a220711d06e241bbcb
JavierQuinan <javierquiandra@gmail.com>
2025-01-06 20:54:27 -0500

    Cambios

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
 
 export default [
-    {
-        files: ["**/*.{js,ts,jsx,tsx}"],
-        languageOptions: {
-            ecmaVersion: "latest",
-            sourceType: "module",
-            globals: globals.browser,
-        },
-        rules: {
-            "no-console": "off",
-        }
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
     }
+  }
 ];
